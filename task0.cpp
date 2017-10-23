f4572bfb04833768dbf836041a6e5ed78e4ec125
Pavlina Cherkezova <32837566+PavlinaCherkezova@users.noreply.github.com>
2017-10-23 14:57:34 +0300

    Add files via upload

---
+++ task0.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   //Task0
+   int a;
+
+   cout<<"a= ";
+   cin>>a;
+
+   if(a%2==0)
+    cout<<"Yes";
+   else cout<<"No";
+
+
+
+   return 0;
+}
---
+++ task1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   //Task0
+   int y;
+   cout<<"y=";
+   cin>>y;
+   if(y%4==0 && y%100!=0)
+    cout<<"Godinata e visokosna";
+   else cout<<"Godinata ne e visokosna";
+   return 0;
+
+
+
+
+}
